dcd372751c20cde488ec3050ff0a71ce127f52df
SzymonKowalczykkkk <117838686+SzymonKowalczykkkk@users.noreply.github.com>
2025-01-22 22:45:07 +0100

    Update redis.py

--- redis.py
+++ redis.py
@@ -1,11 +1,11 @@
-import redis
+
 import csv
-from pymongo import MongoClient
 import geopandas as gpd
 import hashlib
 import pandas as pd
 import glob
 import os
+import redis
 
 r = redis.Redis(
     host='redis-15280.c55.eu-central-1-1.ec2.redns.redis-cloud.com',
@@ -21,15 +21,18 @@ def generuj_klucz(szerokosc, dlugosc):
     return hash_klucz
 
 
-df = pd.read_csv("kody_stacji.csv", sep=';', header=0, names=['Lp', 'kod_stacji', 'nazwa_stacji', 'szerokosc', 'dlugosc', 'wysokosc'])
+df = pd.read_csv(r"C:\Semestr_5\PAG\Projekt-blok-2\kody_stacji.csv", sep=';', header=0, names=['kod_stacji', 'miejscowosc', 'nazwa_stacji', 'szerokosc', 'dlugosc', 'wysokosc'])
 
-print(df.head(50))
+# print(df.head(50))
 
-for index, row in df.iterrows():
-    klucz = generuj_klucz(row['szerokosc'], row['dlugosc'])
-    wartosc = f"{row}"
-    r.set(klucz, wartosc)
+# for index, row in df.iterrows():
+#     klucz = generuj_klucz(row['szerokosc'], row['dlugosc'])
+#     wartosc = f"{row['kod_stacji']}, {row['nazwa_stacji']}"
+#     r.set(klucz, wartosc)   
 
-print(r.get(generuj_klucz('49 59 44', '18 55 09')))
+wynik = r.get(generuj_klucz('49 59 44', '18 55 09'))
+wynik = wynik.split(", ")
 
-print("DONE")
+
+print(f"id: " + wynik[0])
+print(f"name: " + wynik[1])
